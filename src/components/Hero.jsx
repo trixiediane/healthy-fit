f7d9c5e7a3d54f14196d067d980298f2e7fa6416
trixiediane <swiftsklossy490@gmail.com>
2024-10-13 13:06:31 +0800

    Hero Component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
+            <div className='flex flex-col gap-4'>
+                <p>IT'S TIME TO BE</p>
+                <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>HEALTHY <span className='text-blue-400'>
+                    & FIT
+                </span>
+                </h1>
+            </div>
+            <p className='text-sm md:text-base font-light'>
+                I hereby acknowledge that I am embarking on a quest to be both <span className='text-blue-400 font-medium'>
+                    healthy and fit
+                </span>.
+                I understand this may involve moving more than just to the fridge, eating things that aren’t always pizza, and occasionally choosing a salad (no promises). I also acknowledge that being healthy and fit doesn’t mean perfection, but progress—whether it’s running a mile or just running late. <span className='text-blue-400 font-medium'>
+                    In the spirit of balance,
+                </span> I fully embrace that cheat days exist, and naps are a vital part of recovery. Let's get healthy, let's get fit, and let’s keep it fun!
+            </p>
+            <button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
+                <p>Accept & Begin</p>
+            </button>
+        </div>
+    )
+}
